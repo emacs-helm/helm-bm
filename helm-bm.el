a67373effe0a9ad8be42b39b244ac4d01f3c15fc
Thierry Volpiatto <thievol@posteo.net>
2024-07-22 08:24:31 +0200

    Update comments in headers

--- helm-bm.el
+++ helm-bm.el
@@ -27,8 +27,15 @@
 
 ;; Add this file in your `load-path' and the following to your Emacs init file:
 ;;
-;; (autoload 'helm-bm "helm-bm" nil t) ;; Not necessary if using ELPA package
+;; (autoload 'helm-bm "helm-bm" nil t) ;; Not necessary if using ELPA package.
+
+;; Settings:
+
+;; Bind helm-bm to a key of your choice, e.g.
 ;; (global-set-key (kbd "C-c b") 'helm-bm)
+;;
+;; Show bookmarks from all buffers or only from current buffer according
+;; to `bm-cycle-all-buffers' value.
 
 ;;; Code:
 
